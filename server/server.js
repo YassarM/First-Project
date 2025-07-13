e19b823aa6cc0a5695feeb50d7bad1d038f99a7e
YassarM <yassarmuwaffaq93@gmail.com>
2025-07-13 13:14:50 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -33,11 +33,11 @@ app.use(session({
 
 // ✅ Gunakan mysql2/promise
 const mysql = require('mysql2/promise');
-const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "kemal",
+cconst pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
