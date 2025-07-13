40485c1ea6b491b793643b54275b403ac24e2481
YassarM <yassarmuwaffaq93@gmail.com>
2025-07-13 13:15:06 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
