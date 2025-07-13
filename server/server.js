0a5755577169467268bb04135b8e869259b1d4db
YassarM <yassarmuwaffaq93@gmail.com>
2025-07-13 13:27:27 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -221,5 +221,5 @@ app.get('/peserta', async (req, res) => {
 // ✅ START SERVER
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log("Server started on port", PORT);
+    console.log(`Server started on port , ${PORT}`);
 });
