94b1c63f9abba53482192591d4a5b7c2552a3cad
YassarM <yassarmuwaffaq93@gmail.com>
2025-08-06 21:06:03 +0700

    fix: use ESM style postcss config for Vercel

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,6 +1,7 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+/** @type {import('postcss').Config} */
+export default {
+  plugins: [tailwindcss, autoprefixer],
 };
